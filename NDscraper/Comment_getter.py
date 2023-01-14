5b5f5a6b0eaabafab2da314ace243bd591efa2f2
Justin Yeung <100493751+jyeungtin@users.noreply.github.com>
2023-01-15 00:34:23 +0100

    Updated functions

--- NDscraper/Comment_getter.py
+++ NDscraper/Comment_getter.py
@@ -1,50 +1,52 @@
-#Specific function for actual comment as it requires a set of different data structure 
+def find_post_comments():
+    comment = []
+    comment_author = []
+    comment_post_id = []
+    comment_IDs = []
+    comment_neighbourhoods = []
 
-comment = []
-comment_author = []
-comment_post_id = []
-comment_IDs = []
-comment_neighbourhoods = []
+    for i in range(len(post_grid)):
+        try:
+            print(f"Current post: {i}") #Tell us which post the script is processing :)
 
-for i in range(len(post_grid)):
-    try:
-        post_id = post_grid[i].get_attribute("id") #get post id
+            post_id = post_grid[i].get_attribute("id") #get post id
 
-        temp_comment_count = post_grid[i].find_element(By.CLASS_NAME, "css-13yklgh").find_elements(By.CLASS_NAME, "_2kP4d1Rw") #return list of comment (if any)
-        temp_comment_count = len(temp_comment_count) #return num of comment (if any)
-        
-        if temp_comment_count != 0:
-            for comment_num in range(temp_comment_count):
-                try:
-                    ctext_buttons = post_grid[i].find_elements(By.LINK_TEXT, "Meer bekijken") #find expand comment text button
+            temp_comment_count = post_grid[i].find_element(By.CLASS_NAME, "css-13yklgh").find_elements(By.CLASS_NAME, "_2kP4d1Rw") #return list of comment (if any)
+            temp_comment_count = len(temp_comment_count) #return num of comment (if any)
+            
+            if temp_comment_count != 0:
+                for comment_num in range(temp_comment_count):
+                    try:
+                        ctext_buttons = post_grid[i].find_elements(By.LINK_TEXT, "Meer bekijken") #find expand comment text button
 
-                    for button in ctext_buttons:
-                        driver.execute_script("arguments[0].click();", button) #use javascript to click the button
+                        for button in ctext_buttons:
+                            driver.execute_script("arguments[0].click();", button) #use javascript to click the button
 
-                    time.sleep(1) #wait one second for comments to load in
-                except NoSuchElementException:
-                    print("nsee")
-                    pass
+                        time.sleep(1) #wait one second for comments to load in
+                    except NoSuchElementException:
+                        print("nsee")
+                        pass
 
-                comment_ID = post_grid[i].find_elements(By.CLASS_NAME, "js-media-comment")[comment_num].get_attribute("id") #get comment ID of a certain comment
+                    comment_ID = post_grid[i].find_elements(By.CLASS_NAME, "js-media-comment")[comment_num].get_attribute("id") #get comment ID of a certain comment
 
-                comment_text = post_grid[i].find_elements(By.CLASS_NAME, "js-media-comment")[comment_num].find_element(By.CLASS_NAME, "Linkify").text #get comment main text
+                    comment_text = post_grid[i].find_elements(By.CLASS_NAME, "js-media-comment")[comment_num].find_element(By.CLASS_NAME, "Linkify").text #get comment main text
 
-                comment_user = post_grid[i].find_elements(By.CLASS_NAME, "js-media-comment")[comment_num].find_element(By.CLASS_NAME, "comment-detail-author-name").text #get comment's username
+                    comment_user = post_grid[i].find_elements(By.CLASS_NAME, "js-media-comment")[comment_num].find_element(By.CLASS_NAME, "comment-detail-author-name").text #get comment's username
 
-                comment_neighbourhood = post_grid[i].find_elements(By.CLASS_NAME, "js-media-comment")[comment_num].find_element(By.CLASS_NAME, "css-19hrmqv").text #get comment's author's located neighbourhood
+                    comment_neighbourhood = post_grid[i].find_elements(By.CLASS_NAME, "js-media-comment")[comment_num].find_element(By.CLASS_NAME, "css-19hrmqv").text #get comment's author's located neighbourhood
 
-                '''''
-                Append all extracted information to the lists 
-                '''''
+                    '''''
+                    Append all extracted information to the lists 
+                    '''''
 
-                comment.append(comment_text)
-                comment_post_id.append(post_id)
-                comment_author.append(comment_user)
-                comment_IDs.append(comment_ID)
-                comment_neighbourhoods.append(comment_neighbourhood)
-                
-    except NoSuchElementException:  #handle exception where no element indicated was found
-        pass
-    except IndexError:  #handle index error 
-        pass
+                    comment.append(comment_text)
+                    comment_post_id.append(post_id)
+                    comment_author.append(comment_user)
+                    comment_IDs.append(comment_ID)
+                    comment_neighbourhoods.append(comment_neighbourhood)
+                    
+        except NoSuchElementException:  #handle exception where no element indicated was found
+            pass
+        except IndexError:  #handle index error 
+            pass
+    return comment, comment_post_id, comment_author, comment_IDs, comment_neighbourhoods
---
+++ NDscraper/Post_getter.py
@@ -0,0 +1,69 @@
+def find_post_information():
+    #get links from result lists
+    post_grid = driver.find_elements(By.CLASS_NAME, "css-n4lbsh") #pass list of individual posts to post_grid
+
+    post = []
+    text = []
+    neighbourhood_name = []
+    post_username = []
+    comment_count = []
+    post_date = []
+    reaction_count = []
+
+
+    for i in range(len(post_grid)):
+        try: 
+            print(f"Current post: {i}") #Tell us which post the script is processing :)
+
+            #click on  "more comments" and "more content" button and find comments count
+            try:
+                comment_button = post_grid[i].find_element(By.CLASS_NAME, "css-mn71l5").find_element(By.CSS_SELECTOR, 'button') #find more comments button
+                driver.execute_script("arguments[0].click();", comment_button) #use javascript to click the button
+
+                expand_text_button = post_grid[i].find_element(By.CLASS_NAME, "css-zglld0").find_element(By.CSS_SELECTOR, 'button') #find expand text button
+                driver.execute_script("arguments[0].click();", expand_text_button) #use javascript to click the button
+                
+                time.sleep(1) #wait one second for comments to load in
+
+            except NoSuchElementException:
+                pass
+    
+            #get post comment counts 
+            temp_comment_count = post_grid[i].find_element(By.CLASS_NAME, "css-13yklgh").find_elements(By.CLASS_NAME, "css-1qica3u") #return list of comment (if any)
+            comment_count.append(len(temp_comment_count))
+
+            post_id = post_grid[i].get_attribute("id") #get the post id 
+
+            #get post user
+            post_user = post_grid[i].find_element(By.CLASS_NAME, "E7NPJ3WK").text
+
+            #get post text
+            post_text = post_grid[i].find_element(By.CLASS_NAME, "content-body").text #get body text from post[i]
+                
+            #get post date and neighbourhood name
+            byline = post_grid[i].find_elements(By.CLASS_NAME, "post-byline-redesign") #getting two elements from byline
+
+            neighbourhood = byline[0].text #pass the first element to neighbourhood name
+
+            date = byline[1].text #pass the second element to date 
+
+            eng_date = dutch_to_eng(date) #change dutch date to eng date
+
+            changed_date = changedate(eng_date) #reformat date 
+            
+            #get reactions, including likes and other emojis
+            temp_reaction_count = get_reaction(post_grid[i])
+
+            '''''
+            Append all extracted information to the lists 
+            '''''
+            post.append(post_id) 
+            text.append(post_text)
+            neighbourhood_name.append(neighbourhood)
+            post_username.append(post_user)
+            reaction_count.append(temp_reaction_count)
+            post_date.append(changed_date)
+            
+        except NoSuchElementException: #handle exception where no element indicated was found
+            pass
+    return post, text, neighbourhood_name, post_username, reaction_count, post_date, comment_count
